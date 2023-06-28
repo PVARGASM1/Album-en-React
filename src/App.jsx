94be03ee636a95f4110acf9826e8a1a53a1a0812
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-06-27 22:35:35 -0500

    Resolve the album-react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import ComponentAlbum from '../components/ComponentAlbum'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ComponentAlbum/>
+      </div>
+    </>
+  )
+}
+
+export default App
