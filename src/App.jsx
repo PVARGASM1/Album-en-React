ca4943015aaf4d455f0ac2440a3cff183c3e3db3
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-07-04 12:06:21 -0500

    Correction of album react, finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import './App.css'
-import ComponentAlbum from '../components/ComponentAlbum'
+import React, { useState } from "react";
+import "./App.css";
+import Album from "../components/ComponentAlbum";
+import Title from "../components/Title/Title";
+import canciones from "./data/canciones";
 
 function App() {
-
+  const [songs, setSongs] = useState(canciones);
   return (
-      <div>
-        <ComponentAlbum/>
-      </div>
-  )
+    <div>
+      <Title
+        text="Album de Musica"
+        numSongs={songs.length}
+        liked={songs.filter((cancion) => cancion.like).length}
+      />
+      <Album
+        canciones={songs}
+        replaceSongs={(updatedSongs) => setSongs(updatedSongs)}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
